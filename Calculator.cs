8af30991d82088cc5db783cbec0e793f0b3fc730
stam <simchabalay@gmail.com>
2022-06-14 23:20:02 +0300

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_App
+{
+    public class Calculator
+    {
+        public static Double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public static Double Subtract(double x, double y)
+        {
+            return x - y;
+        }
+
+        public static Double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+
+        public static Double Divide(double x, double y)
+        {
+            if (y != 0)
+            {
+                return x / y;
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileSystemWatcher watcher = new FileSystemWatcher(@"C:\Users\Queen B\Desktop\simcha");
+            watcher.EnableRaisingEvents = true;
+            watcher.IncludeSubdirectories = true;
+
+
+
+            watcher.Created += FileSystemWatcher_Created;
+
+            watcher.Renamed += FileSystemWatcher_Renamed;
+
+            watcher.Deleted += FileSystemWatcher_Deleted;
+
+            watcher.EnableRaisingEvents = true;
+
+            Console.ReadKey();
+        }
+
+   //     static void FileSystemWatcher_Created(object sender, FileSystemEventArgs e)
+   //     {
+   //         Console.WriteLine("File: {0} , created at time/* {1}*/ ", e.Name/*, e.OldName*/);
+   //     }
+   //static void FileSystemWatcher_Renamed(object sender, FileSystemEventArgs e)
+   //     {
+   //         Console.WriteLine("File: {0} , renamed to {1} at time {2}", e.Name/*, e.OldName*/, DateTime.Now.ToLocalTime());
+   //     }
+
+   //     static void FileSystemWatcher_Deleted(object sender, FileSystemEventArgs e)
+
+   //     {
+   //         Console.WriteLine("File: {0} , deleted at time: {1} ", e.Name, DateTime.Now.ToLocalTime());
+   //     }
+   //     static void FileSystemWatcher_Changed(object sender, FileSystemEventArgs e)
+
+   //     {
+   //         Console.WriteLine("File: {0} , change at time: {1} ", e.Name, DateTime.Now.ToLocalTime());
+
+   //     }
+    
+        static void FileSystemWatcher_Created(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("File: {0} ,", e.Name);
+        }
+
+        static void FileSystemWatcher_Renamed(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("File: {0} ,at time {1}", e.Name, DateTime.Now.ToLocalTime());
+        }
+
+        static void FileSystemWatcher_Deleted(object sender, FileSystemEventArgs e)
+
+        {
+            Console.WriteLine("File: {0} , deleted at time: {1} ", e.Name, DateTime.Now.ToLocalTime());
+        }
+        static void FileSystemWatcher_Changed(object sender, FileSystemEventArgs e)
+
+        {
+            Console.WriteLine("File: {0} , change at time: {1} ", e.Name, DateTime.Now.ToLocalTime());
+        }
+    }
+}
